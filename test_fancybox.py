1183c6c1961516d0364a9ccf39d22ed49a06aed4
anselmos <anselmos@users.noreply.github.com>
2017-04-12 15:20:19 +0200

    Changes name of function to assert_*
    This makes more common sense than creating Return information and then
    in code add "assert is_*".

--- test_fancybox.py
+++ test_fancybox.py
@@ -19,16 +19,16 @@ def register():
     "Registers plugin"
     signals.article_generator_finalized.connect(fancybox_plugin)
 
-def is_receiver_registered(receiver_name):
+def assert_receiver_registered(receiver_name):
     'Checks if receiver is registered to signals generator'
-    return signals.article_generator_finalized.has_receivers_for(receiver_name)
+    assert signals.article_generator_finalized.has_receivers_for(receiver_name)
 
 def test_plugin_registers():
     """
     Check if plugin registers to article_generator_finalized
     """
     register()
-    assert is_receiver_registered(fancybox_plugin)
+    assert_receiver_registered(fancybox_plugin)
 
 def test_article_generator_return_article():
     """
