fc3ecf1fde83002e8847dbd2ff23fc7c458e4821
anselmos <anselmos@users.noreply.github.com>
2017-04-12 14:35:19 +0200

    Cleans code for test_plugin_registers

--- test_fancybox.py
+++ test_fancybox.py
@@ -11,9 +11,13 @@ def register():
     "Registers plugin"
     signals.article_generator_finalized.connect(fancybox_plugin)
 
+def is_receiver_registered(receiver_name):
+    'Checks if receiver is registered to signals generator'
+    return signals.article_generator_finalized.has_receivers_for(receiver_name)
+
 def test_plugin_registers():
     """
     Check if plugin registers to article_generator_finalized
     """
     register()
-    assert signals.article_generator_finalized.has_receivers_for(fancybox_plugin)
+    assert is_receiver_registered(fancybox_plugin)
