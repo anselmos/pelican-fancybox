2c1bf5c91c5f67580b2ed64f949ee481058b533d
anselmos <anselmos@users.noreply.github.com>
2017-04-12 14:29:51 +0200

    Adds first test if plugin is connected to signals.

--- test_fancybox.py
+++ test_fancybox.py
@@ -1,9 +1,19 @@
 """
 FancyBox unittests
 """
+from pelican import signals
 
-def test_nothing():
-    """
-    Nothing
-    """
+def fancybox_plugin():
+    "Fanxybox plugin - temporary code placement"
     pass
+
+def register():
+    "Registers plugin"
+    signals.article_generator_finalized.connect(fancybox_plugin)
+
+def test_plugin_registers():
+    """
+    Check if plugin registers to article_generator_finalized
+    """
+    register()
+    assert signals.article_generator_finalized.has_receivers_for(fancybox_plugin)
