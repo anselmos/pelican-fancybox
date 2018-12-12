42ff7902b4bc4a3599e6d7eec3b9ea9adfd84a46
Geert Stappers <stappers@stappers.nl>
2018-12-12 21:39:12 +0100

    documentation.py: document strings wrapped
    All lines are now shorter as 80 characters.
    ( because PEP8 says max line lenghth < 80 )

--- documentation.py
+++ documentation.py
@@ -5,21 +5,25 @@ Fancybox unittests
 
 def test_include_dependencies():
     """
-    Tests if output from function "include_dependencies" will create javascript elements as a html-output
+    Tests if output from function "include_dependencies" will create
+    javascript elements as a html-output
     """
     raise NotImplementedError()
 
 
 def test_fancybox_plugin_script():
     """
-    Test if function creates proper javascript-script output for fancybox activation on .fancybox class
+    Test if function creates proper javascript-script output for
+    fancybox activation on .fancybox class
     """
     raise NotImplementedError()
 
 
 def test_get_all_fancybox_markdown_elements():
     """
-    This test will check if function called "get_all_fancybox_markdown_elements" returns properly all "fancybox" elements
+    This test will check if function called
+    "get_all_fancybox_markdown_elements" returns properly
+    all "fancybox" elements
     """
     raise NotImplementedError()
 
@@ -27,21 +31,25 @@ def test_get_all_fancybox_markdown_elements():
 def test_generate_fancybox_link():
     """
     Test for function generate_fancybox_link.
-    Checks if this function creates proper link based on markdown information provided
+    Checks if this function creates proper link based on
+    markdown information provided
     """
     raise NotImplementedError()
 
 
 def test_parse_fancybox():
     """
-    Function parse_fancybox will use "get_all_fancybox_markdown_elements" and use output from this function to replace in content information with a link
+    Function parse_fancybox will use "get_all_fancybox_markdown_elements"
+    and use output from this function to replace in content information
+    with a link
     """
     raise NotImplementedError()
 
 
 def test_article_generator_finalized():
     """
-    This tests if main fancybox function for lightbox mechanism is working properly.
+    This tests if main fancybox function for lightbox mechanism
+    is working properly.
     Will check if output of this fuction creates proper content at the end
     """
     raise NotImplementedError()
